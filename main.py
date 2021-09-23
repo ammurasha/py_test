e6fbf8c411e24abad14e0c33310335ece89d9811
ammurasha <ammurasha@gmail.com>
2021-09-23 22:44:07 +0300

    create main

---
+++ main.py
@@ -0,0 +1,20 @@
+import math
+
+a = 10
+for i in range(1, 7):
+    print(i)
+    a = a * i
+    print(a)
+
+
+def ttt2():
+    print('ttt2')
+
+
+def ttt1():
+    print('ttt1')
+
+
+ttt2()
+ttt1()
+ttt2()
