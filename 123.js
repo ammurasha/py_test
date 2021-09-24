df50bcdb304cbcec36879f7df220432881568ee3
ammurasha <ammurasha@gmail.com>
2021-09-24 16:23:27 +0300

    add JS and DB

---
+++ 123.js
@@ -0,0 +1,31 @@
+const block= document.getElementById('block')
+console.dir(block.textContent)
+
+setTimeout(()=>{
+  addStyles1To(block)
+},2500)
+
+block.onclick = () => {
+  addStyles2To(block)
+}
+
+block.addEventListener('dblclick', () => {
+  addStyles1To(block)
+})
+
+function addStyles1To(temp) {
+  temp.textContent = 'edited with JS'
+  temp.style.color='#006699'
+  temp.style.backgroundColor='black'
+}
+
+function addStyles2To(temp) {
+  temp.textContent = 'after click'
+if (temp.style.backgroundColor==='red') {
+  temp.style.color='#006699'
+  temp.style.backgroundColor='yellow'
+} else {
+  temp.style.color='#FFFF00'
+  temp.style.backgroundColor='red'
+}
+}
