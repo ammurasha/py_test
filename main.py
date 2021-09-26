386b01f0bdfbdda8083621414a00646c088391cb
ammurasha <ammurasha@gmail.com>
2021-09-26 13:40:18 +0300

    RegEx match

--- main.py
+++ main.py
@@ -20,8 +20,15 @@ ttt()
 ttt1()
 ttt()
 
-result = re.findall(r'\w+', 'AV is largest Analytics community of India')
+result = re.findall(r'\w+', 'AV is largest Analyt1cs commun1ty of India')
 print(result)
 
-emails = re.findall(r'@(\w+.\w+)', 'abc.test@gmail.com, xyz@test.in, test.first@analyticsvidhya.com, first.test@rest.biz')
+emails = re.findall(r'\b((\S+(?=@))@((?<=@)\S+))\b', 'abc.test@gmail.com, xyz@test.in, test.first@analyticsvidhya.com, first.test@rest.biz')
 print(emails)
+
+pattern = r'((\d)(\d))((\d)(\d))'
+string = r'123456789'
+match = re.search(pattern, string)
+print(f'Найдена подстрока >{match[0]}< с позиции {match.start(0)} до {match.end(0)}')
+for i in range(0, 7):
+    print(f'Группа №{i} >{match[i]}< с позиции {match.start(i)} до {match.end(i)}')
